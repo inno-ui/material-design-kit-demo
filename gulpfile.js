2796c717da61dc717fe7d6608135fa501c6577b5
inno-ui <onuvlad95@gmail.com>
2021-01-31 15:09:46 +0000

    Gulp Minify CSS
    -

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
 var gulp = require('gulp');
 var open = require('gulp-open');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
@@ -10,9 +12,11 @@ sass.compiler = require('node-sass');
 
 var filePaths = {
 	CSS: './assets/css',
+	CSS_COMPILED: './assets/css/inno-ui-kit.css',
+	CSS_MINIFIED: 'inno-ui-kit.min.css',
 	ENTRY_POINT: './index.html',
 	SCSS: './assets/scss/**/*.scss',
-	SCSS_COMPILED: './assets/scss/inno-ui-demo.scss'
+	SCSS_COMPILED: './assets/scss/inno-ui-kit.scss'
 };
 
 gulp.task('compile-scss', function () {
@@ -24,8 +28,15 @@ gulp.task('compile-scss', function () {
 	.pipe(gulp.dest(filePaths.CSS));
 });
 
+gulp.task('minify-css', () => {
+	return gulp.src(filePaths.CSS_COMPILED)
+	.pipe(cleanCSS())
+	.pipe(rename(filePaths.CSS_MINIFIED))
+	.pipe(gulp.dest(filePaths.CSS));
+});
+
 gulp.task('watch-scss', function() {
-	gulp.watch(filePaths.SCSS, gulp.series('compile-scss'));
+	gulp.watch(filePaths.SCSS, gulp.series('compile-scss', 'minify-css'));
 });
 
 gulp.task('open-app', function() {
