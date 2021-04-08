dff5f5e4c5d5041f40b421345aa44139ed46b32b
inno-ui <onuvlad95@gmail.com>
2021-04-09 00:20:59 +0100

    Full project refactoring
    inno.css
    inno.js
    added _close.scss
    added _timepicker.scss
    refactor index.html
    refactor documentation.html

---
+++ assets/js/inno-ui-kit.js
@@ -0,0 +1,26 @@
+$("#basicModalEx").on("shown.bs.modal", function() {
+	$("#myInput").focus()
+})
+
+// tooltip initialization
+$(function() {
+	$('[data-toggle="tooltip"]').tooltip()
+})
+
+// popover initialization
+$(function() {
+	$('[data-toggle="popover"]').popover()
+})
+
+// waves initialization + loading waves script
+$(document).ready(function() {
+	Waves.init();
+	Waves.attach('.btn-rotating', ['waves-circle', 'waves-light']);
+	Waves.attach('.btn:not(.btn-link):not(.btn-light):not(.btn-white):not(.aqua-gradient):not([class*="btn-outline-"]):not([class*="btn-text-"]):not([data-notification-counter]), .btn-floating', ['waves-light']);
+	Waves.attach('.btn-link, .btn-light, .btn-white, .btn.aqua-gradient, [class*="btn-outline-"], [class*="btn-text-"]', ['waves-effect']);
+	Waves.attach('.chip', ['waves-effect']);
+	Waves.attach('.material-icons.action-icon', ['waves-effect']);
+	Waves.attach('.navbar-dark .navbar-nav .nav-item', ['waves-light']);
+	Waves.attach('.navbar-light .navbar-nav .nav-item', ['waves-effect']);
+	Waves.attach('.page-link', ['waves-effect']);
+});
